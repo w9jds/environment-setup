3789cc18ca6e9da05367415fbc0f70eb669f7a17
Jeremy Shore <xboxmusicn3rd@gmail.com>
2020-05-31 11:44:50 -0700

    Fix spaceship location and nvm

--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+ZSH_CUSTOM = '~/.oh-my-zsh'
+
 add_package_sources() {
   # Add the Cloud SDK distribution URI as a package source
   echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
   curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 }
 
-install_upgrades() {
+install_essentials() {
   sudo apt-get update
   sudo apt-get -y upgrade
-  sudo apt-get -y install build-essential
+  sudo apt-get -y install curl git build-essential
 }
 
 setup_zsh_prompt() {
@@ -18,12 +20,12 @@ setup_zsh_prompt() {
   RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
   # Install Spaceship Prompt
-  sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-  sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+  git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+  ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
   # Install ZSH Plugins
-  sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-  sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
   cp zshrc ~/.zshrc
 }
 
@@ -36,8 +38,7 @@ setup_vim() {
 setup_node() {
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-  # Install Node LTS
-  source ~/.bashrc
+  sudo bash ~/.nvm/nvm.sh
   nvm install --lts
 }
 
@@ -53,7 +54,7 @@ setup_firebase() {
 
 main() {
   add_package_sources
-  install_upgrades
+  install_essentials
   setup_vim
   
   setup_zsh_prompt
